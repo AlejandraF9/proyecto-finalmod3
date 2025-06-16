b313efec0eda48f679699efe2ce8c2c44b694727
Alejandra <alejandra.f.c.95@gmail.com>
2025-06-16 14:35:03 +0100

    Created folder -project-finalmod3- via VITE, deleted unnecessary folders and files, created algorithms folder and JavaScript files of exercises 1 to 5

---
+++ algoritmos/ejercicio1.js
@@ -0,0 +1,15 @@
+
+
+
+
+
+
+
+
+// git add .
+
+// git commit -m "Primer commit": creación de carpeta proyecto-finalmod3 a través de VITE, eliminación de carpetas y archivos innecesarios, creación de carpeta algoritmos y arvhicos de ejercicios del 1 al 5
+
+// git push -u origin main/master
+
+//README.md
